036e92d89465f1d593f9137a5882f7d019d598ea
Bramar <suhar_bramar@yahoo.com>
2026-08-09 16:56:47 +0700

    Feature: Add ReactRouter

---
+++ client/src/pages/DashboardPage.js
@@ -0,0 +1,10 @@
+const DashBoardPage = () => {
+  return (
+    <main className="page">
+      <h2>Dashboard</h2>
+      <p>Welcome to your income and expense tracker</p>
+    </main>
+  );
+};
+
+export default DashBoardPage;
